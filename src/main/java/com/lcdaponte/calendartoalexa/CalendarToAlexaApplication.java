73add5e5179ac38525a0304728cc4aa92a624868
Lucas Cavalcante <lcdaponte@gmail.com>
2023-06-04 11:05:35 -0300

    feat: initial commit

---
+++ src/main/java/com/lcdaponte/calendartoalexa/CalendarToAlexaApplication.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.calendartoalexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarToAlexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarToAlexaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcdaponte/calendartoalexa/CalendarToAlexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.calendartoalexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarToAlexaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
