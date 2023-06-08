33fcacd1e9c3f6082d71c6d0299fccbbf0093cad
Lucas Cavalcante <lcdaponte@gmail.com>
2023-06-08 20:11:30 -0300

    feat: POC Google OAUTH2 and Calendar

---
+++ src/main/java/com/lcdaponte/calendartoalexa/application/controller/GoogleCalendarController.java
@@ -0,0 +1,66 @@
+package com.lcdaponte.calendartoalexa.application.controller;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+@RestController
+public class GoogleCalendarController {
+
+    private static final String APPLICATION_NAME = "Google Calendar To Alexa";
+    private final Credential credential;
+
+    private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    @Autowired
+    public GoogleCalendarController(final Credential credential) {
+        this.credential = credential;
+    }
+
+    @GetMapping("/calendar")
+    public ResponseEntity<String> getCalendar() throws GeneralSecurityException, IOException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service =
+                new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                        .setApplicationName(APPLICATION_NAME)
+                        .build();
+
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+        List<Event> items = events.getItems();
+        if (items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        } else {
+            System.out.println("Upcoming events");
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    start = event.getStart().getDate();
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start);
+            }
+        }
+
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
